d2c25ae5512619e6a336a1d30cfe2c244bb81025
ban11 <ttqc9nr9@gmail.com>
2018-10-21 13:51:56 +0900

    q3

--- q3/src/main/scala/q3.scala
+++ q3/src/main/scala/q3.scala
@@ -1,41 +1,50 @@
 import java.io.File
 
-import org.biojava.nbio.core.sequence.{DNASequence, ProteinSequence}
-import org.biojava.nbio.core.sequence.compound.AmbiguityDNACompoundSet
-
+import org.biojava.nbio.core.sequence.DNASequence
+import org.biojava.nbio.core.sequence.io.FastaReaderHelper
 object genomeCounter{
-  implicit def str2DNA(seq: String) = new DNASequence(seq,AmbiguityDNACompoundSet.getDNACompoundSet)
 
-  def getListofFile(dir: String,filePattern: scala.util.matching.Regex): List[File] ={
+  def getListofFile(dir: String,filePattern: String): List[File] ={
     val d = new File(dir)
     if (d.exists) {
       val files = d.listFiles
                    .filter(_.isFile)
-                   .filter(_ match {
-                                    case `filePattern` => true
-                                    case _ => false
-                                   })
+                   .filter(_.toString.matches(filePattern)) 
                    .toList
       files
     }else{
+      println("none")
       List[File]()
     }
   }
-
+  
+  def getFastaFiles(files: List[File]) = {
+  }
       
 
   def main(args:Array[String]): Unit = {
-    val dirGenome = "/bio/db/fasta/genome"
-    val genomeFile = """.*\.genome$""".r
-    
-    val genomeList = getListofFile(dirGenome,genomeFile)
-    println(genomeList.length)
+    val dirGenome = "/bio/db/fasta/genome/"
+    val genomeFile = """.*\.genome$"""
 
-    val dirGene ="/bio/db/fasta/gene"
-    val geneFile = """.*\.nuc$""".r
+    val genomeList = getListofFile(dirGenome,genomeFile)
+                    .map( f => FastaReaderHelper.readFastaDNASequence(f))
+      
+    println(genomeList)
+    
+    
+
+    val dirGene ="/bio/db/fasta/genes/"
+    val geneFile = """.*\.nuc$""" 
 
     val geneList =getListofFile(dirGene,geneFile)
     println(geneList.length)
+
+
+
+
+
+
+
 
   }
 }
